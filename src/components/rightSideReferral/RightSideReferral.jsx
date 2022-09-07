1b4fec830f2d12ac75c2c3f68a70b4dba77f7631
Mgibson7 <mgibsonasc@gmail.com>
2022-09-07 10:07:39 -0400

    added slideshow player for referrals

--- src/components/rightSideReferral/RightSideReferral.jsx
+++ src/components/rightSideReferral/RightSideReferral.jsx
@@ -1,59 +1,39 @@
 
 import "./rightSideReferral.css"
+import SimpleImageSlider from "react-simple-image-slider";
 
 const RightSideReferral = () => {
+  const images = [
+    { url: "ref.jpg" },
+    { url: "ref2.jpg" },
+    { url: "ref3.jpg" },
+  ];
+  const width = "25%";
+  const height = "25%";
+  
   
   return (
     <div className = "rightSide">
-    <div className="testimonialTests">
-      <div className="testimonialOne">
-      <p>Impressed with what he's been able to accomplish and learn in this field on his own. Fast learner and dedicated to enhancing his skillset daily. Highly motivated and I believe he'd be an excellent fit for any team!
-          </p>
-          <div className="person">
-          <h3> - CHRIS MURPHY, SOFTWARE ENGINEER</h3>
-          <img src="./ibm.png" alt="" />
-            </div>
-          
-          
-          </div>
 
-      <div className="testimonialOne testimonialTwo">
-      <p>Planning, coordinating, and motivating are just a few of the skills Michael has mastered to provide a positive end user experience for subscribers.
-        Michael has a talent for keeping all the pieces of a process moving in the right direction and can multitask with ease. 
-          </p>
-          <div className="person">
-          <h3> - SUNEIL CHANDER, STRATEGIC GROWTH MANAGER NATIONAL PARTNERSHIPS</h3>
-          <img src="./viasat.png" alt="" />
-            </div>
-          
-          </div>
-        <div className="testimonialOne testimonialThree">
-          <p>Needed an app to blast incoming project alerts out to my contractors in real time. 
-            Michael created it in less than 24 hours and showed me how to use the app easily on both my desktop and phone. 
-            Also was there to offer support or minor modications any time they were requested. 
-            Additionally, he performed data analysis on our customer base to reveal our top ROI in respect to ads and customer types.
-            His overall work offered a great boost to my start up in its early stages! 
-          </p>
-          <div className="person">
-          <h3> - ARNOLD FERREIRAS, CEO JUNK GURUS</h3>
-          <img src="./junk.JPG" alt="" />
-            </div>
-
-
-        </div>
-       
       
-        
+      <div className="simpleImageSlide">
+      <SimpleImageSlider 
+      className = "simpleImage"
+        width={width}
+        height={height}
+        images={images}
+        showBullets={false}
+        showNavs={false}
+        loop = {true}
+        autoPlay = {true}
+        slideDuration = {3.5}
+      />
+
+      </div>
+      
+
     
-  
-
-</div>
-
       
-
-
-
-
     
       
     </div>
