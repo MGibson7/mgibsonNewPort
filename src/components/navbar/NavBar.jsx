e81438743fddf9f2b119b458c99f7e2f1432739b
Mgibson7 <mgibsonasc@gmail.com>
2022-09-28 09:03:54 -0400

    added favicon and link on logo

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom"
 import "./navbar.css"
 
 const NavBar = () => {
   return (
     <div className="navMain">
-        <h1>MCG</h1>
+      <Link to = "/">
+      <h1>MCG</h1>
         <h1 className="port">PORTFOLIO</h1>
+      </Link>
+        
 
     </div>
   )
